8a50c0299e12ffbcc256c5dcf82be292d96754cd
duraidur <30861359+duraidur@users.noreply.github.com>
2019-06-27 18:23:18 +0530

    ok

---
+++ mylibrary/src/main/java/com/durai/marpar/mylibrary/Durai.java
@@ -0,0 +1,16 @@
+package com.durai.marpar.mylibrary;
+
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+public class Durai {
+
+
+    public  void addition(int a,int b){
+        Log.i(TAG, "addition: "+(a+b));
+    }
+    public void subtraction(int a,int b){
+        Log.i(TAG, "subtraction: "+(a-b));
+    }
+}
